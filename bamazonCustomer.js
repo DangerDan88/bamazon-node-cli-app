d182a2539a8fa7470cecb2cc0ec786101c0b52a2
Danger <51889207+DangerDan88@users.noreply.github.com>
2019-10-31 20:49:15 -0600

    database connected

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,34 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+  
+ 
+    user: "root",
+
+    password: "yourRootPassword",
+    database: "bamazon"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+
+  });
+
+
+  function startQuestion(){
+      inquirer
+      .prompt({
+          name: "start",
+          message: "What would you like to search?",
+          choices: [
+
+          ]
+      })
+  }
+  
