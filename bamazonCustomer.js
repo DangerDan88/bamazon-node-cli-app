9b9e100b73048eaca1b64889397afc4ed580d83f
Danger <51889207+DangerDan88@users.noreply.github.com>
2019-11-02 12:42:18 -0600

    starter file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,34 +1,34 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
 
-    port: 3306,
-  
- 
-    user: "root",
+  port: 3306,
 
-    password: "yourRootPassword",
-    database: "bamazon"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+  user: "root",
 
-  });
+  password: "yourRootPassword",
+  database: "bamazon"
+});
 
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  startQuestion();
+});
 
-  function startQuestion(){
-      inquirer
-      .prompt({
-          name: "start",
-          message: "What would you like to search?",
-          choices: [
+function startQuestion() {
+  inquirer
+    .prompt({
+      name: "start",
+      message: "What is the id of the product you would like to buy?"
+    })
 
-          ]
-      })
-  }
-  
+    .then(function(answer) {
+      var query = "SELECT products, item_id, FROM bamazon WHERE ?";
+      connection.query(query, function(err, res) {
+        console.log(res);
+      });
+    });
+}
