8bd3b9909163f84a5f2762488c19bd5776f33ae0
Danger <51889207+DangerDan88@users.noreply.github.com>
2019-11-07 17:08:47 -0700

    fixed bugs trying to run questions in order

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,9 +15,21 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   // console.log("connected as id " + connection.threadId);
-  startDisplay();
-  displayItemsChosen();
+  startQuestion();
+  // displayItemsChosen();
 });
+
+function startQuestion() {
+  inquirer
+    .prompt({
+      name: "start",
+      message: "Are you ready to check out this awesome CLI app?"
+    })
+
+    .then(function(answer) {
+      startDisplay(answer);
+    });
+}
 
 function displayItemsChosen() {
   inquirer
@@ -27,33 +39,28 @@ function displayItemsChosen() {
     })
 
     .then(function(answer) {
-      connection.query("SELECT * FROM products WHERE item_id = ?",[answer.items] ,function(err,response) {
-        for (var i = 0; i < response.length; i++) {
-          console.log(response[i].product_name);
-          console.log("Item id is  "+ response[i].item_id);
-        console.log(response[i].price);
-        console.log(response[i].stock_quanity);
+      connection.query(
+        "SELECT * FROM products WHERE item_id = ?",
+        [answer.items],
+        function(err, response) {
+          for (var i = 0; i < response.length; i++) {
+            console.log(response[i].product_name);
+            console.log("Item id is  " + response[i].item_id);
+            console.log("Price of item  " + "$" + response[i].price);
+            console.log("Stock quanity  " + response[i].stock_quanity);
+          }
         }
-      });
+      );
     });
 }
 
-function startDisplay() {
-  inquirer
-    .prompt({
-      name: "start",
-      message: "Are you ready to check out this awesome CLI app?",
-      type: "Boolean"
-    })
-
-    .then(function(answer) {
-      var query = "SELECT * FROM products";
-
-      connection.query(query, [answer.start], function(err, response) {
-        for (var i = 0; i < response.length; i++) {
-         // console.log(response[i].product_name);
-         // console.log(response[i].item_id);
-        }
-      });
-    });
+function startDisplay(answer) {
+  var query = "SELECT * FROM products";
+  connection.query(query, [answer.start], function(err, response) {
+    console.log("Here are the items available.!");
+    for (var i = 0; i < response.length; i++) {
+      console.log(response[i].product_name);
+      console.log(response[i].item_id);
+    }
+  });
 }
