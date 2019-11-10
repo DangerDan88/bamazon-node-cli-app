c8a1c34a4725dbdaad0f4a06241d8601bca39814
Danger <51889207+DangerDan88@users.noreply.github.com>
2019-11-10 14:42:47 -0700

    final still need something to console.log new quanitiy

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,7 +15,6 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   // console.log("connected as id " + connection.threadId);
-
 });
 
 function homePage() {
@@ -32,31 +31,70 @@ function homePage() {
     }
   });
   shopping();
+}
+
+var shopping = function() {
+  inquirer
+    .prompt({
+      name: "productToBuy",
+      type: "input",
+      message: "Please enter the Product Id of the item you wish to purchase.?!"
+    })
+    .then(function(answer1) {
+      var selection = answer1.productToBuy;
+      connection.query(
+        "SELECT * FROM products WHERE item_id=?",
+        selection,
+        function(err, res) {
+          if (err) throw err;
+          if (res.length === 0) {
+            console.log(
+              "That Product doesn't exist, Please enter a Product Id from the list above"
+            );
+
+            shopping();
+          } else {
+            inquirer
+              .prompt({
+                name: "quantity",
+                type: "input",
+                message: "How many items would you like to purchase?"
+              })
+              .then(function(answer2) {
+                var quantity = answer2.quantity;
+                if (quantity > res[0].stock_quanity) {
+                  console.log(
+                    "Our Apologies we only have " +
+                      res[0].stock_quantity +
+                      " items of the product selected"
+                  );
+                  shopping();
+                } else {
+                  console.log("");
+                  console.log(res[0].product_name + " purchased");
+                  console.log(quantity + " qty @ $" + res[0].price);
+
+                  var newQuantity = res[0].stock_quanity - quantity;
+                  connection.query(
+                    "UPDATE products SET stock_quanity = ",
+                    newQuantity,
+                    " WHERE stock_quanity = " + res[0].item_id,
+                    function(err, resUpdate) {
+                      if (err) throw err;
+                      console.log("");
+                      console.log("Your Order has been Processed");
+                      console.log("Thank you for Shopping with us...!");
+                      console.log("");
+                      // console.log(resUpdate);
+                      connection.end();
+                    }
+                  );
+                }
+              });
+          }
+        }
+      );
+    });
 };
 
-function shopping() {
-  inquirer.prompt(
-    {
-      name: "productBuy",
-      type: "input",
-      message: "What is the item id of the product you would like to purchase?"
-    }
-  ).then(function(answer1) {
-    var selection = answer1.productBuy;
-    connection.query(
-      "SELECT * FROM products WHERE item_id=?",
-      selection,
-      function(err, response) {
-        if (err) throw err;
-        if (response.length === 0) {
-          console.log(
-            "That Product doesn't exist, Please enter a Product Id from the list above"
-          );
-
-          shopping();
-        } 
-      }
-    );
-  });
-}
 homePage();
